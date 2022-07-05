8925db18b63bcb89d3c785dff85eb6e8333ce294
yeon-bo <dylk97@gamil.com>
2022-07-06 05:45:44 +0900

    Create & Fix  form.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Form from './components/Form';
+import Form from './pages/Form';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
